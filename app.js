2a1f0780b5a5d31a49da13e4fef4f12e3536ee08
MateuTorres <mateutorresnoguera@gmail.com>
2026-03-05 11:40:57 +0100

    Añadimos nuevas funcionalidades

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const taskInput = document.getElementById("taskInput")
 const addBtn = document.getElementById("addBtn")
 const taskList = document.getElementById("taskList")
+const taskCounter = document.getElementById("taskCounter")
+const filterButtons = document.querySelectorAll(".filters button")
+const darkModeBtn = document.getElementById("darkModeBtn")
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+let currentFilter = "all"
 
 function saveTasks(){
 localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+function updateCounter(){
+
+const pending = tasks.filter(task => !task.completed).length
+taskCounter.textContent = pending
+
 }
 
 function renderTasks(){
 
 taskList.innerHTML=""
 
-tasks.forEach((task,index)=>{
+let filteredTasks = tasks
+
+if(currentFilter==="pending"){
+filteredTasks = tasks.filter(task=>!task.completed)
+}
+
+if(currentFilter==="completed"){
+filteredTasks = tasks.filter(task=>task.completed)
+}
+
+filteredTasks.forEach((task,index)=>{
 
 const li=document.createElement("li")
 
@@ -25,10 +46,8 @@ li.innerHTML=`
 <span>${task.text}</span>
 
 <div class="task-buttons">
-
-<button class="complete-btn">✓</button>
-<button class="delete-btn">✕</button>
-
+<button class="complete-btn">✔</button>
+<button class="delete-btn">✖</button>
 </div>
 
 `
@@ -37,13 +56,13 @@ const completeBtn = li.querySelector(".complete-btn")
 const deleteBtn = li.querySelector(".delete-btn")
 
 completeBtn.onclick=()=>{
-tasks[index].completed=!tasks[index].completed
+task.completed=!task.completed
 saveTasks()
 renderTasks()
 }
 
 deleteBtn.onclick=()=>{
-tasks.splice(index,1)
+tasks.splice(tasks.indexOf(task),1)
 saveTasks()
 renderTasks()
 }
@@ -51,6 +70,8 @@ renderTasks()
 taskList.appendChild(li)
 
 })
+
+updateCounter()
 
 }
 
@@ -77,5 +98,18 @@ addBtn.addEventListener("click",addTask)
 taskInput.addEventListener("keypress",(e)=>{
 if(e.key==="Enter") addTask()
 })
+
+filterButtons.forEach(button=>{
+
+button.onclick=()=>{
+currentFilter = button.dataset.filter
+renderTasks()
+}
+
+})
+
+darkModeBtn.onclick=()=>{
+document.body.classList.toggle("dark-mode")
+}
 
 renderTasks()
